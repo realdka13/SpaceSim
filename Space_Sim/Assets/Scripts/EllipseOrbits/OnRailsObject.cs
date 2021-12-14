da45d237d0d44eb4473cadb0044b2e33726201e2
Donovan <magne069@umn.edu>
2021-12-14 12:10:09 -0600

    Ellipse Orbits Complete

--- Space_Sim/Assets/Scripts/EllipseOrbits/OnRailsObject.cs
+++ Space_Sim/Assets/Scripts/EllipseOrbits/OnRailsObject.cs
@@ -9,37 +9,43 @@ public class OnRailsObject : MonoBehaviour
 
     //Orbital Keplerian Parameters
     [Header("Keplerian Parameters")]
-    [SerializeField]                            float semiMajorAxis = 20f;      //a - size
-    [SerializeField] [Range(0f, 0.95f)]         float eccentricity;             //e - shape
-    [SerializeField] [Range(0f, TAU)]           float inclination = 0f;         //i - tilt
-    [SerializeField] [Range(0f, TAU)]           float longitudeOfAcendingNode;  //Ω - swivel
-    [SerializeField] [Range(0f, TAU)]           float argumentOfPeriapsis;      //w - orbit position
+    [SerializeField]                            float semiMajorAxis = 20f;      //a - Size
+    [SerializeField] [Range(0f, 0.95f)]         float eccentricity;             //e - Shape
+    [SerializeField] [Range(0f, TAU)]           float inclination = 0f;         //i - Tilt
+    [SerializeField] [Range(0f, TAU)]           float longitudeOfAcendingNode;  //Ω - Swivel
+    [SerializeField] [Range(0f, TAU)]           float argumentOfPeriapsis;      //w - Rotation
 
-    [Header("Position Offset")]
-    [SerializeField]                            float meanLongitude;            //L - body offset     **TODO Set slider limits
+    [Header("Body Position Offset")]
+    [SerializeField]                            float meanLongitude;            //L - Body Position Offset
     
-    [Space]
     [Header("Accuracy Settings")]
     [SerializeField] float accuracyTolerance = 1e-6f;
     [SerializeField] int maxIterations = 5;           //usually converges after 3-5 iterations.
 
+    [Header("Orbit Drawing")]
+    [SerializeField] bool redrawEveryFrame = false;    //Useful for bodies such as moons which orbit around a planet that is orbiting another body
+    [SerializeField] int orbitResolution = 50;
+    [SerializeField] [Range(0f,1)] float lineWidth = .25f;
+
     [Space]
     [SerializeField] OnRailsReferenceBody referenceBody;
+    LineRenderer lineRender;
 
     //Numbers which only change if orbit or mass changes
-    [HideInInspector] [SerializeField] float mu, n, trueAnomalyConstant, cosLOAN, sinLOAN, sinI, cosI; //**TODO Move these to only be calculated once
+    [HideInInspector] [SerializeField] float mu, n, trueAnomalyConstant, cosLOAN, sinLOAN, sinI, cosI; //
 
 
     void Awake()
     {
         CalculateSemiConstants();
+        DrawOrbit();
     }
 
     void Update()
     {
         //---Calculating Mean Anomaly (M)---
         //Calulate n here..
-        float meanAnomaly = (float)(n * (Time.time - meanLongitude));      //M=M0+n*dt, mean longitude which is offsetting the objects starting position
+        float meanAnomaly = (float)(n * (Time.time + meanLongitude));      //M=M0+n*dt, mean longitude which is offsetting the objects starting position
 
 
 
@@ -83,12 +89,15 @@ public class OnRailsObject : MonoBehaviour
         float y = distance * (sinI * sinAOPPlusTA);
 
         transform.position = new Vector3(x, y, z) + referenceBody.transform.position;
+
+        if(redrawEveryFrame)
+        {
+            DrawOrbit();
+        }
     }
 
     void CalculateSemiConstants()
     {
-        Debug.Log("Calculated Constants");
-
         //For Calculating mean anomaly
         mu = Universe.G * referenceBody.mass;                       //Standard Gravitational Paramter
         n = Mathf.Sqrt(mu / Mathf.Pow(semiMajorAxis, 3));           //Mean Angular Motion n=sqrt(mu/a^3)
@@ -103,8 +112,74 @@ public class OnRailsObject : MonoBehaviour
         sinI = Mathf.Sin(inclination);
     }
 
+    void DrawOrbit()
+    {
+        //CalculateSemiConstants must always be called before this
+        Vector3[] orbitalPoints = new Vector3[orbitResolution + 1];
+        if (referenceBody == null)
+        {
+            Debug.LogWarning($"Add a reference body to {gameObject.name}");
+            return;
+        }
+        else
+        {
+            Vector3 pos = referenceBody.transform.position;     //Get position of reference body
+            float orbitFraction = 1f / orbitResolution;         //breakup orbit into chunks
+
+            //Calculate Points
+            for (int i = 0; i < orbitResolution + 1; i++)
+            {
+                float EccentricAnomaly = i * orbitFraction * TAU;
+
+                float trueAnomaly = 2 * Mathf.Atan(trueAnomalyConstant * Mathf.Tan(EccentricAnomaly / 2));
+                float distance = semiMajorAxis * (1 - eccentricity * Mathf.Cos(EccentricAnomaly));
+
+                float cosAOPPlusTA = Mathf.Cos(argumentOfPeriapsis + trueAnomaly);
+                float sinAOPPlusTA = Mathf.Sin(argumentOfPeriapsis + trueAnomaly);
+
+                float x = distance * ((cosLOAN * cosAOPPlusTA) - (sinLOAN * sinAOPPlusTA * cosI));
+                float z = distance * ((sinLOAN * cosAOPPlusTA) + (cosLOAN * sinAOPPlusTA * cosI));
+                float y = distance * (sinI * sinAOPPlusTA);
+
+                float meanAnomaly = EccentricAnomaly - eccentricity * Mathf.Sin(EccentricAnomaly);
+
+                orbitalPoints[i] = pos + new Vector3(x, y, z);
+            }
+
+            lineRender = GetComponent<LineRenderer>();
+            lineRender.startWidth = lineWidth;
+            lineRender.endWidth = lineWidth;
+            lineRender.positionCount = orbitResolution + 1;
+            lineRender.SetPositions(orbitalPoints);
+        }
+    }
+
+    void RecalculateBodyPosition()
+    {
+        float meanAnomaly = (float)(n * (0 + meanLongitude));      //M=M0+n*dt, mean longitude which is offsetting the objects starting position
+        float E1 = meanAnomaly;
+        float difference = 1f;
+        for (int i = 0; difference > accuracyTolerance && i < maxIterations; i++)
+        {
+            float E0 = E1;
+            E1 = E0 - (meanAnomaly - E0 + eccentricity * Mathf.Sin(E0)) / ((-1f) + eccentricity * Mathf.Cos(E0));
+            difference = Mathf.Abs(E1 - E0);
+        }
+        float EccentricAnomaly = E1;
+        float trueAnomaly = 2 * Mathf.Atan(trueAnomalyConstant * Mathf.Tan(EccentricAnomaly / 2)); //f=2*arctan((1+e)/(1-e)*tan(E/2))
+        float distance = semiMajorAxis * (1 - eccentricity * Mathf.Cos(EccentricAnomaly));
+        float cosAOPPlusTA = Mathf.Cos(argumentOfPeriapsis + trueAnomaly);
+        float sinAOPPlusTA = Mathf.Sin(argumentOfPeriapsis + trueAnomaly);
+        float x = distance * ((cosLOAN * cosAOPPlusTA) - (sinLOAN * sinAOPPlusTA * cosI));
+        float z = distance * ((sinLOAN * cosAOPPlusTA) + (cosLOAN * sinAOPPlusTA * cosI));
+        float y = distance * (sinI * sinAOPPlusTA);
+        transform.position = new Vector3(x, y, z) + referenceBody.transform.position;
+    }
+
     private void OnValidate() 
     {
         CalculateSemiConstants();
+        RecalculateBodyPosition();
+        DrawOrbit();
     }
 }
--- Space_Sim/Assets/Scripts/Universe.cs
+++ Space_Sim/Assets/Scripts/Universe.cs
@@ -6,6 +6,6 @@ using UnityEngine;
 //A class designed to hold the universal constants
 public static class Universe {
     public const float gravitationalConstant = 0.0001f;     //For Newton
-    public const float G = 6.67f;                           //For Kepler
+    public const float G = .667f;                           //For Kepler
     public const float physicsTimeStep = 0.01f;
 }
