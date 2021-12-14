939a1aef2de01f4e4806625abcc4de9c34011f1f
Donovan <magne069@umn.edu>
2021-12-14 10:09:28 -0600

    Optimized by only calculating constantly changing values

--- Space_Sim/Assets/Scripts/EllipseOrbits/OnRailsObject.cs
+++ Space_Sim/Assets/Scripts/EllipseOrbits/OnRailsObject.cs
@@ -13,7 +13,7 @@ public class OnRailsObject : MonoBehaviour
     [SerializeField] [Range(0f, 0.95f)]         float eccentricity;             //e - shape
     [SerializeField] [Range(0f, TAU)]           float inclination = 0f;         //i - tilt
     [SerializeField] [Range(0f, TAU)]           float longitudeOfAcendingNode;  //Ω - swivel
-    [SerializeField] [Range(0f, TAU)]           float argumentOfPeriapsis;      //w - orbit position  **TODO Figure this out and change name accordingly
+    [SerializeField] [Range(0f, TAU)]           float argumentOfPeriapsis;      //w - orbit position
 
     [Header("Position Offset")]
     [SerializeField]                            float meanLongitude;            //L - body offset     **TODO Set slider limits
@@ -27,14 +27,20 @@ public class OnRailsObject : MonoBehaviour
     [SerializeField] OnRailsReferenceBody referenceBody;
 
     //Numbers which only change if orbit or mass changes
-    [HideInInspector] [SerializeField] float mu, n, cosLOAN, sinLOAN, sinI, cosI, trueAnomalyConstant;
+    [HideInInspector] [SerializeField] float mu, n, trueAnomalyConstant, cosLOAN, sinLOAN, sinI, cosI; //**TODO Move these to only be calculated once
+
+
+    void Awake()
+    {
+        CalculateSemiConstants();
+    }
 
     void Update()
     {
         //---Calculating Mean Anomaly (M)---
-        mu = Universe.G * referenceBody.mass;                                           //Standard Gravitational Paramter
-        n = Mathf.Sqrt(mu / Mathf.Pow(semiMajorAxis, 3));                               //Mean Angular Motion n=sqrt(mu/a^3)
-        float meanAnomaly = (float)(n * (Time.time - meanLongitude));                   //M=M0+n*dt, mean longitude which is offsetting the objects starting position
+        //Calulate n here..
+        float meanAnomaly = (float)(n * (Time.time - meanLongitude));      //M=M0+n*dt, mean longitude which is offsetting the objects starting position
+
 
 
         /*
@@ -53,10 +59,12 @@ public class OnRailsObject : MonoBehaviour
         float EccentricAnomaly = E1;
 
 
+
         //---Calculating True Anomaly (f)---
-        trueAnomalyConstant = Mathf.Sqrt((1 + eccentricity) / (1 - eccentricity));
+        //Calculating true anomaly constant here...
         float trueAnomaly = 2 * Mathf.Atan(trueAnomalyConstant * Mathf.Tan(EccentricAnomaly / 2)); //f=2*arctan((1+e)/(1-e)*tan(E/2))
         float distance = semiMajorAxis * (1 - eccentricity * Mathf.Cos(EccentricAnomaly));
+
 
 
         /*
@@ -66,17 +74,37 @@ public class OnRailsObject : MonoBehaviour
             Z=r*(sin(i)*sin(w+f))
         */
 
-        cosLOAN = Mathf.Cos(longitudeOfAcendingNode);
-        sinLOAN = Mathf.Sin(longitudeOfAcendingNode);
-        cosI = Mathf.Cos(inclination);
-        sinI = Mathf.Sin(inclination);
-
+        //Calculate sin/cos consts here...
         float cosAOPPlusTA = Mathf.Cos(argumentOfPeriapsis + trueAnomaly);
         float sinAOPPlusTA = Mathf.Sin(argumentOfPeriapsis + trueAnomaly);
+
         float x = distance * ((cosLOAN * cosAOPPlusTA) - (sinLOAN * sinAOPPlusTA * cosI));
         float z = distance * ((sinLOAN * cosAOPPlusTA) + (cosLOAN * sinAOPPlusTA * cosI));
         float y = distance * (sinI * sinAOPPlusTA);
 
         transform.position = new Vector3(x, y, z) + referenceBody.transform.position;
+    }
+
+    void CalculateSemiConstants()
+    {
+        Debug.Log("Calculated Constants");
+
+        //For Calculating mean anomaly
+        mu = Universe.G * referenceBody.mass;                       //Standard Gravitational Paramter
+        n = Mathf.Sqrt(mu / Mathf.Pow(semiMajorAxis, 3));           //Mean Angular Motion n=sqrt(mu/a^3)
+
+        //For Calculating true anomaly
+        trueAnomalyConstant = Mathf.Sqrt((1 + eccentricity) / (1 - eccentricity));
+
+        //For adding the orbit to 3D space
+        cosLOAN = Mathf.Cos(longitudeOfAcendingNode);
+        sinLOAN = Mathf.Sin(longitudeOfAcendingNode);
+        cosI = Mathf.Cos(inclination);
+        sinI = Mathf.Sin(inclination);
+    }
+
+    private void OnValidate() 
+    {
+        CalculateSemiConstants();
     }
 }
