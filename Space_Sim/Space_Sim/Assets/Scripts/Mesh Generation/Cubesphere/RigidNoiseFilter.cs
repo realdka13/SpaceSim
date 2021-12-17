26a4908334ba459ca1fbd33e107c7eed84970931
Donovan <magne069@umn.edu>
2021-12-16 22:45:54 -0600

    Rigid Noise Filter Added

--- Space_Sim/Space_Sim/Assets/Scripts/Mesh Generation/Cubesphere/RigidNoiseFilter.cs
+++ Space_Sim/Space_Sim/Assets/Scripts/Mesh Generation/Cubesphere/RigidNoiseFilter.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoiseFilter {
-
-    NoiseSettings settings;
+public class RigidNoiseFilter : INoiseFilter
+{
+    NoiseSettings.RigidNoiseSettings settings;
     Noise noise = new Noise();
 
-    public NoiseFilter(NoiseSettings settings)
+    public RigidNoiseFilter(NoiseSettings.RigidNoiseSettings settings)
     {
         this.settings = settings;
     }
@@ -18,10 +18,16 @@ public class NoiseFilter {
         float frequency = settings.baseRoughness;
         float amplitude = 1;
 
+        float weight = 1;
+
         for (int i = 0; i < settings.numLayers; i++)
         {
-            float v = noise.Evaluate(point * frequency + settings.centre);
-            noiseValue += (v + 1) * .5f * amplitude;
+            float v = 1 - Mathf.Abs(noise.Evaluate(point * frequency + settings.centre));
+            v *= v;
+            v *= weight;
+            weight = Mathf.Clamp01(v * settings.weightMultiplier);
+
+            noiseValue += v * amplitude;
             frequency *= settings.roughness;
             amplitude *= settings.persistence;
         }
