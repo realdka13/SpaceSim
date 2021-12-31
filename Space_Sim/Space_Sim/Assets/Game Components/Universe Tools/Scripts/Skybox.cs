0fb96d77e6ef7d1d3dab0296cfd718f3345db5d6
Donovan <magne069@umn.edu>
2021-12-30 20:12:32 -0600

    Reorganized

---
+++ Space_Sim/Space_Sim/Assets/Game Components/Universe Tools/Scripts/Skybox.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skybox : MonoBehaviour
+{
+    //Universe Manaegr
+    private UniverseManager universeManager;
+
+    //Children
+    [SerializeField]
+    private GameObject skyboxCamera;
+
+    //Skybox
+    [SerializeField]
+    private float skyboxScale = 1f; //This number indicates how much bigger the skybox appears than its actual size
+
+    //Bodies
+    [SerializeField]
+    private GameObject[] railBodyMeshes;    // TODO Sync this with Universe Manager
+
+//**********************************************************************************************************************
+
+    private void Awake()
+    {
+        universeManager = GameObject.FindGameObjectWithTag("Universe Manager").GetComponent<UniverseManager>();
+    }
+
+    private void Start()
+    {
+        //railedBodies = new GameObject[BodiesIndex];   // TODO this will be done automatically later, when prefabs of the planets exist
+    }
+
+    private void Update()
+    {
+        //Update Camera
+        skyboxCamera.transform.rotation = universeManager.GetPlayerRotation();
+        skyboxCamera.transform.localPosition = universeManager.GetPlayerCoords() / skyboxScale;
+
+        //Update bodies
+        for (int i = 0; i < railBodyMeshes.Length; i++)
+        {
+            Vector3 railBodyCoords = universeManager.GetBodyCoords(i);
+            railBodyMeshes[i].transform.localPosition = railBodyCoords / skyboxScale;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, 1100f);
+    }
+}
---
+++ Space_Sim/Space_Sim/Assets/Game Components/Universe Tools/Scripts/UniverseManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniverseManager : MonoBehaviour
+{
+    //Origin
+    private Vector3d currentOrigin;
+
+    //Player
+    private Transform playerTransform;
+    private Vector3d playerUniverseCoords;
+
+    //For Teleporting
+    [Space,Header("Teleport To:")]
+    [SerializeField]
+    private double playerXCoord;
+    [SerializeField]
+    private double playerYCoord;
+    [SerializeField]
+    private double playerZCoord;
+
+    //Railed Bodies
+    [Space,SerializeField]
+    private RailBody[] railBodies;
+
+//*****************************************************************************************************************************************************
+
+    void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        for (int i = 0; i < railBodies.Length; i++)
+        {
+            railBodies[i].CalculateSemiConstants(); // TODO change mass
+        }
+    }
+
+    void Update()
+    {
+        //Get player position
+        playerUniverseCoords.Set((double)playerTransform.position.x + currentOrigin.x, (double)playerTransform.position.y + currentOrigin.y, (double)playerTransform.position.z + currentOrigin.z);
+        for (int i = 0; i < railBodies.Length; i++)
+        {
+            railBodies[i].CalculateCoordinates(Time.time); // TODO Will be upgraded to a universal timer
+        }
+    }
+
+    private void OnValidate()   // TODO Later move this to in game debug?
+    {
+        //For Teleporting
+        currentOrigin.Set(playerXCoord,playerYCoord,playerZCoord);
+        for (int i = 0; i < railBodies.Length; i++)
+        {
+            railBodies[i].CalculateSemiConstants(); // TODO change mass
+        }
+    }
+
+
+    //This function will update the current origin position to use in calculating the players universe coords
+    public void UpdateOrigin(Vector3 originOffset)
+    {
+        currentOrigin.Set(currentOrigin.x + (double)originOffset.x, currentOrigin.y + (double)originOffset.y, currentOrigin.z + (double)originOffset.z);
+    }
+
+    public Vector3 GetPlayerCoords()
+    {
+        return new Vector3((float)playerUniverseCoords.x, (float)playerUniverseCoords.y, (float)playerUniverseCoords.z);
+    }
+
+    public Quaternion GetPlayerRotation()
+    {
+        return playerTransform.rotation;
+    }
+
+    public int GetBodyCount()
+    {
+        return railBodies.Length;
+    }
+
+    public Vector3 GetBodyCoords(int bodyIndex)
+    {
+        Vector3d railBodyCoords = railBodies[bodyIndex].GetCoordinates();
+        return new Vector3((float)railBodyCoords.x, (float)railBodyCoords.y, (float)railBodyCoords.z);
+    }
+}
